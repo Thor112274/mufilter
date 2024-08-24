51b2949349aba634a69981a17b89499a6499e3fe
Thor112274 <126616021+Thor112274@users.noreply.github.com>
2024-08-25 02:06:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,16 @@
 # Don't Remove Credit @movie_file_20
 # Subscribe YouTube Channel For Amazing Bot @movie_file_20
 # Ask Doubt on telegram @KingVJ01
+
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/ELINA-BOTS/Neha.git /Neha
+  git clone https://github.com/Thor112274/filter1.git /filter1
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Neha
 fi
-cd /Neha
+cd /filter1
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
