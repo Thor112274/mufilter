e03bd5ec3f0243b5e83fc8f2e2f5f9071a485b6e
Thor112274 <126616021+Thor112274@users.noreply.github.com>
2024-08-25 01:58:43 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 # Don't Remove Credit @movie_file_20
 # Subscribe YouTube Channel For Amazing Bot @movie_file_20
 # Ask Doubt on telegram @KingVJ01
-
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
